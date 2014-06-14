b25e5eeb708f17aea8b58a3f8782af47f59dc094
Janith Bandara <janith3000@gmail.com>
2014-06-14 21:26:37 +0530

    added changes to io

--- src/main/java/org/audit4j/core/io/HandlerWriter.java
+++ src/main/java/org/audit4j/core/io/HandlerWriter.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2014 Janith Bandara, This source is a part of Audit4j - 
+ * An open-source audit platform for Enterprise java platform.
+ * http://mechanizedspace.com/audit4j
+ * http://audit4j.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.audit4j.core.io;
 
 import java.util.ArrayList;
@@ -38,11 +57,10 @@ public class HandlerWriter implements AuditOutputStream {
      */
     @Override
     public HandlerWriter write(AuditEvent event) {
-        System.out.println("pool" + pool);
         for (Class<? extends Handler> handlerClazz : handlers) {
             Handler handler = pool.checkOut(handlerClazz);
             handler.setAuditEvent(event);
-            handler.setQuery(buildQuery(event.getActionItems(), event.getAction()));
+            handler.setQuery(buildQuery(event.getEventElements(), event.getAction()));
             handler.handle();
             pool.checkIn(handler);
         }
@@ -108,5 +126,10 @@ public class HandlerWriter implements AuditOutputStream {
         } else {
             return "No data for selectred audit criteria";
         }
+    }
+    
+    @Override
+    public Object clone(){
+    	return null;
     }
 }
