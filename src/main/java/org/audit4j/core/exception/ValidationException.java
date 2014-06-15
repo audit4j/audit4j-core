26608c4bd1116d00663255350dad1775691c69dc
Janith Bandara <janith3000@gmail.com>
2014-06-15 13:15:06 +0530

    added exception classes

---
+++ src/main/java/org/audit4j/core/exception/ValidationException.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2014 Janith Bandara, This source is a part of Audit4j - 
+ * An open-source audit platform for Enterprise java platform.
+ * http://mechanizedspace.com/audit4j
+ * http://audit4j.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.audit4j.core.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Class ValidationException.
+ *
+ * @author <a href="mailto:janith3000@gmail.com">Janith Bandara</a>
+ */
+public class ValidationException extends RuntimeException{
+	
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 3273908609152516053L;
+	
+	/** The log. */
+	private static final Logger LOG = LoggerFactory.getLogger(ValidationException.class);
+
+	/** The Constant VALIDATION_LEVEL_WARN. */
+	public static final String VALIDATION_LEVEL_WARN = "WARN";
+	
+	/** The Constant VALIDATION_LEVEL_INVALID. */
+	public static final String VALIDATION_LEVEL_INVALID = "INVALID";
+			
+	/** The level. */
+	private final String level;
+	
+	/**
+	 * Instantiates a new validation exception.
+	 *
+	 * @param message the message
+	 * @param level the level
+	 */
+	public ValidationException(final String message, final String level){
+		super(message);
+		System.out.println("Audit4j: Validation Exception: " + message);
+		this.level = level;
+		//LOG.warn("Validation Exception: " + message);
+	}
+
+	/**
+	 * Gets the level.
+	 *
+	 * @return the level
+	 */
+	public String getLevel() {
+		return level;
+	}
+
+}
